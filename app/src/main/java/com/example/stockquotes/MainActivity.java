a5220c4780df1f9e7197c783765029dde63c4968
Hollande <hollandepowell@gmail.com>
2020-04-14 16:51:51 -0400

    Horizontal layout and search history set up

--- app/src/main/java/com/example/stockquotes/MainActivity.java
+++ app/src/main/java/com/example/stockquotes/MainActivity.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import edu.cofc.stock.Stock;
 
@@ -27,10 +28,15 @@ TextView lastTradePrice;
 TextView lastTradeTime;
 TextView change;
 TextView range;
+Button hist1;
+Button hist2;
+Button hist3;
+Button submitBtn;
 boolean retrievalFailed = false;
 Toast toast;
 SharedPreferences sharedPref;
 SharedPreferences.Editor editor;
+String[] history = new String[3];
 
 
 
@@ -38,9 +44,12 @@ SharedPreferences.Editor editor;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Create sharedPref
         sharedPref =this.getPreferences(Context.MODE_PRIVATE);
         editor = sharedPref.edit();
 
+        //Instantiate View objects and restore saved states
         final EditText stockInput = findViewById(R.id.stockInput);
         stockSymbol = findViewById(R.id.stockSymbol);
         stockSymbol.setText(sharedPref.getString("stockSymbol", getString(R.string.blank)));
@@ -55,22 +64,66 @@ SharedPreferences.Editor editor;
         range = findViewById(R.id.range);
         range.setText(sharedPref.getString("range", getString(R.string.blank)));
 
-
+        //Prepare Toast object for invalid stock input
         toast = makeText(getApplicationContext(), "Invalid Stock Symbol", Toast.LENGTH_SHORT);
 
-        Button submitBtn = findViewById(R.id.submitBtn);
-
-
+        submitBtn = findViewById(R.id.submitBtn);
         submitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 symbol = stockInput.getText().toString();
-                StockRetrievalTask test = new StockRetrievalTask();
-                test.execute(symbol);
+                cycleHistory(symbol);
+                new StockRetrievalTask().execute(symbol);
             }
         });
 
+        //Set up previous search buttons
+        history[0] = sharedPref.getString("hist1", "N/A");
+        history[1] = sharedPref.getString("hist2", "N/A");
+        history[2] = sharedPref.getString("hist3", "N/A");
+        hist1 = findViewById(R.id.history1);
+        hist1.setText(history[0]);
+        hist2 = findViewById(R.id.history2);
+        hist2.setText(history[1]);
+        hist3 = findViewById(R.id.history3);
+        hist3.setText(history[2]);
 
+        hist1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                symbol = hist1.getText().toString();
+                new StockRetrievalTask().execute(symbol);
+            }
+        });
+
+        hist2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                symbol = hist2.getText().toString();
+                new StockRetrievalTask().execute(symbol);
+            }
+        });
+
+        hist3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                symbol = hist3.getText().toString();
+                new StockRetrievalTask().execute(symbol);
+            }
+        });
+    }
+
+    private void cycleHistory(String symbol){
+        history[2] = history[1];
+        history[1] = history[0];
+        history[0] = symbol;
+        hist1.setText(history[0]);
+        hist2.setText(history[1]);
+        hist3.setText(history[2]);
+        editor.putString("hist1", history[0]);
+        editor.putString("hist2", history[1]);
+        editor.putString("hist3", history[2]);
+        editor.commit();
     }
 
     private class StockRetrievalTask extends AsyncTask<String, Void, Stock>{
